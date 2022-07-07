bd7bc51a440aa2534261ad733688ccf5a1337787
Anthony Zhen <anthony20151128@gmail.com>
2022-07-07 11:37:26 -0400

    stdout

--- main.js
+++ main.js
@@ -5,13 +5,25 @@ const app = express()
 const PORT = process.env.PORT
 
 app.get('/deploy/:name', (req, res) => {
-  res.status(200).send(`Deploy request received, name: ${req.params.name}`)
-  exec(`sudo docker pull ${req.params.name}:latest`)
-  console.log(`Pulling latest docker ${req.params.name} image...`)
-  exec(`sudo docker restart ${req.params.name}:latest`)
-  console.log(`Restartubg docker container ${req.params.name}:latest`)
+    res.status(200).send(`Deploy request received, name: ${req.params.name}`)
+    exec(`sudo docker pull ${req.params.name}:latest`, (error, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+    })
+    console.log(`Pulling latest docker ${req.params.name} image...`)
+    exec(`sudo docker restart ${req.params.name}:latest`, (error, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+    })
+    console.log(`Restartubg docker container ${req.params.name}:latest`)
 })
 
 app.listen(PORT, () => {
-  console.log(`Deploy webhook listening at http://localhost:${PORT}`)
+    console.log(`Deploy webhook listening at http://localhost:${PORT}`)
 })
