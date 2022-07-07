4f7fc771d8c2f1dc9e6ba9c87129ef437f36f07d
Anthony Zhen <anthony20151128@gmail.com>
2022-07-07 11:25:33 -0400

    deploy

---
+++ main.js
@@ -0,0 +1,17 @@
+const { exec } = require("child_process");
+const express = require('express')
+require("dotenv").config();
+const app = express()
+const PORT = process.env.PORT
+
+app.get('/deploy/:name', (req, res) => {
+  res.status(200).send(`Deploy request received, name: ${req.params.name}`)
+  exec(`sudo docker pull ${req.params.name}:latest`)
+  console.log(`Pulling latest docker ${req.params.name} image...`)
+  exec(`sudo docker restart ${req.params.name}:latest`)
+  console.log(`Restartubg docker container ${req.params.name}:latest`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Deploy webhook listening at http://localhost:${PORT}`)
+})
