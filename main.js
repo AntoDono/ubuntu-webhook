eb54ea0367ea86dc6f1bad7512013823d98840d1
Anthony Zhen <anthony20151128@gmail.com>
2022-07-19 08:46:05 -0400

    port

--- main.js
+++ main.js
@@ -16,10 +16,13 @@ async function bash(file_path, arg) {
   };
 };
 
-app.get('/deploy/:name/:package/:version', async(req, res) => {
+app.get('/deploy/:name/:package/:version/:port', async(req, res) => {
     let name = req.params.name
     let package = req.params.package
     let version = req.params.version
+    let port = req.params.port
+
+    if (!name || !package || !version || !port) res.status(400).send("Missing arguments")
 
     try{
         console.log(`[Redeploy Sys]: Fetching for pacakage ID and name of ${name}/${package}:${version} image`)
@@ -37,11 +40,11 @@ app.get('/deploy/:name/:package/:version', async(req, res) => {
         await bash('./scripts/pull.sh', `-n ${name} -p '${package}' -v '${version}'`)
     
         console.log(`[Redeploy Sys]: Starting docker container ${name}/${package}:${version}`)
-        await bash('./scripts/start.sh', `-n ${name} -p '${package}' -v '${version}'`)
+        await bash('./scripts/start.sh', `-n ${name} -p '${package}' -v '${version}' -o '${port}'`)
 
     }catch(err){
         console.error(`[ ERROR ]: ${err}\n\nAbandoning current request.`);
-        res.status(400)
+        res.status(400).send("Server error")
     }
 
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,11 @@
-while getopts n:p:v: flag
+while getopts n:p:v:o flag
 do 
     case "${flag}" in
         n) name=${OPTARG};;
         p) package=${OPTARG};;
         v) version=${OPTARG};;
+        o) port=${OPTARG};;
     esac
 done
 
-docker run -d --name $package $name/$package:$version;
+docker run -d -p $port:$port --name $package $name/$package:$version;
