e2d84ac35de90e53889cdcd5ad033ff95b801bff
Anthony Zhen <anthony20151128@gmail.com>
2022-07-27 12:26:55 -0400

    wtf

--- main.js
+++ main.js
@@ -17,13 +17,13 @@ async function bash(file_path, arg) {
   };
 };
 
-app.post('/deploy', async(req, res) => {
+app.post('/deploy:name', async(req, res) => {
     // let name = req.body.name
     // let package = req.body.package
     // let version = req.body.version
     // let port = req.body.port
 
-    console.log(req.body)
+    console.log(req.params)
     console.log(req.headers)
     console.log(req.data)
 
